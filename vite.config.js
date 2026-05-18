5d6a21577ec03d2559de2d54b9e94fe61b4410ff
soha0219 <cgq@tutanota.com>
2026-05-18 11:41:02 +0800

    fix: 修复打包路径，扁平化文件结构

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,21 @@
-
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src/web',
-
-  // 修改这里：改为 './' 即可，它会自动适配任何路径
-  base: './', 
-
+  root: 'src/web', // 保持这一行
+  base: './',      // 保持相对路径
   build: {
     outDir: '../../dist',
     emptyOutDir: true,
-  },
-
-  server: {
-    open: true,
+    rollupOptions: {
+      output: {
+        // 强制所有生成的 JS 和 CSS 都在 dist 根目录，不要进子文件夹
+        entryFileNames: 'app.js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
   },
 });
-
-
-
-
 
 
 
